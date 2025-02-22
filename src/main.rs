6e7f8c3392de9b34051483d0d4609c324ffa2a98
A-cream <akfain@proton.me>
2025-02-22 10:24:45 +0000

    Ctrl+C doesnt exit the shell

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
+use libc::{signal, tcgetattr, tcsetattr, termios, ECHOCTL, SIGINT, SIG_IGN, TCSANOW};
 mod run;
 mod shell;
 mod terminal;
-use libc::{signal, SIGINT, SIG_IGN};
 
 fn main() {
     unsafe {
         signal(SIGINT, SIG_IGN);
     }
 
-    shell::run();
+    let mut term: termios = unsafe { std::mem::zeroed() };
+    unsafe {
+        tcgetattr(libc::STDIN_FILENO, &mut term);
+    }
+
+    let original_term = term;
+    term.c_lflag &= !ECHOCTL;
+
+    unsafe {
+        tcsetattr(libc::STDIN_FILENO, TCSANOW, &term);
+    }
+
+    let exit_code = shell::run();
+
+    unsafe {
+        tcsetattr(libc::STDIN_FILENO, TCSANOW, &original_term);
+    }
+
+    std::process::exit(exit_code);
 }
